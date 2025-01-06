48e5f27d82c1616c6ff6505d2ffd8211f405d91d
Proclamus <patrick.yasmim@gmail.com>
2025-01-06 18:14:35 -0300

    exercises done

--- Exercises_CSharp/Program.cs
+++ Exercises_CSharp/Program.cs
@@ -54,26 +54,28 @@ namespace Exercises_CSharp
                 }
             }
 
-            static int FlippingMatrix(List<List<int>> matrix)
-            {
-                int sum = 0;
-                int n = matrix.Count;
 
-                for (int i = 0; i < n / 2; i++)
+        }
+
+        public static int FlippingMatrix(List<List<int>> matrix)
+        {
+            int sum = 0;
+            int n = matrix.Count;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
                 {
-                    for (int j = 0; j < n / 2; j++)
-                    {
-                        sum +=
-                            Math.Max(matrix[i][j],
-                                (Math.Max(matrix[i][n - 1 - j],
-                                        (Math.Max(matrix[n - 1 - i][j], matrix[n - 1 - i][n - 1 - j]))
-                                    )
+                    sum +=
+                        Math.Max(matrix[i][j],
+                            (Math.Max(matrix[i][n - 1 - j],
+                                    (Math.Max(matrix[n - 1 - i][j], matrix[n - 1 - i][n - 1 - j]))
                                 )
-                            );
-                    }
+                            )
+                        );
                 }
-                return sum;
             }
+            return sum;
         }
 
 
